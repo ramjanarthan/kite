00a4aca5b8bc251c1fc7496c920237c0cbcc5da6
Ram Janarthan <ram.janarthan11@gmail.com>
2025-02-16 17:34:20 +0000

    Increase length of generated text

--- generate_text.py
+++ generate_text.py
@@ -2,16 +2,16 @@ from custom_model import CustomLanguageModel
 from constants import *
 from primitives import Bigram
 
-python_model = CustomLanguageModel.load_model(python_model_file) # CustomLanguageModel(processed_train_files[0])
-cpp_model = CustomLanguageModel.load_model(cpp_model_file) # CustomLanguageModel(processed_train_files[1])
+python_model =  CustomLanguageModel(processed_train_files[0])
+cpp_model =  CustomLanguageModel(processed_train_files[1])
 
-python_output = python_model.generate_output_sequence(400) #, Bigram("d", "e"))
+python_output = python_model.generate_output_sequence(400, Bigram("d", "e"))
 
 # write python_output to output/generated_python.txt
 with open('output/generated_python.txt', 'w+') as f:
 	f.write(python_output)
 
-cpp_output = cpp_model.generate_output_sequence(400) #, Bigram("#", "i"))
+cpp_output = cpp_model.generate_output_sequence(400, Bigram("#", "i"))
 
 # write python_output to output/generated_python.txt
 with open('output/generated_cpp.txt', 'w+') as f:
